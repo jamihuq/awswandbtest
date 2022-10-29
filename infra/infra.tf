401a2203b59acc5e38baf2ac69aace6d5d086c2b
jamihuq <jamihuq2002@gmail.com>
2022-10-29 02:56:41 -0500

    added HTTPS listeners

--- infra/infra.tf
+++ infra/infra.tf
@@ -44,10 +44,10 @@ resource "aws_acm_certificate" "ziah_dev" {
   validation_method         = "DNS"
 }
 
-data "aws_route53_zone" "ziah_dev" {
-  name         = "ziah_dev.com"
-  private_zone = false
-}
+#data "aws_route53_zone" "ziah_dev" {
+#  name         = "ziah_dev.com"
+#  private_zone = false
+#}
 
 ###########################################################
 # End 
