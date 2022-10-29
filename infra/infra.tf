2d3ec842747179079c936e032e27a545591b7e51
jamihuq <jamihuq2002@gmail.com>
2022-10-29 02:02:04 -0500

    added HTTPS listeners

--- infra/infra.tf
+++ infra/infra.tf
@@ -33,25 +33,41 @@ resource "aws_route53_record" "wandb" {
   records = ["${aws_lb.wandb.dns_name}"]
 }
 
+###########################################################
+# Create Certificate
+# this code is from https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/acm_certificate
+# Begin
+###########################################################
 resource "aws_acm_certificate" "ziah_dev" {
   domain_name       = "ziah_dev.com"
   validation_method = "DNS"
+
+  tags = {
+    Environment = "test"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 data "aws_route53_zone" "ziah_dev" {
   name         = "ziah_dev.com"
   private_zone = false
 }
+###########################################################
+# End 
+###########################################################
 
 
 resource "aws_route53_record" "ziah_dev" {
-#  for_each = {
-#   for dvo in aws_acm_certificate.ziah_dev.domain_validation_options : dvo.domain_name => {
-#      name    = dvo.resource_record_name
-#      record  = dvo.resource_record_value
-#      type    = dvo.resource_record_type
-#    }
-#  }
+  for_each = {
+   for dvo in aws_acm_certificate.ziah_dev.domain_validation_options : dvo.domain_name => {
+      name    = dvo.resource_record_name
+      record  = dvo.resource_record_value
+      type    = dvo.resource_record_type
+    }
+  }
 
   allow_overwrite = true
   name            = each.value.name
