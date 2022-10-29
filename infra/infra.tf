17dda7eb44574ed3fcf710c8b1d7f717a0ad00b2
jamihuq <jamihuq2002@gmail.com>
2022-10-29 01:43:00 -0500

    added HTTPS listeners

--- infra/infra.tf
+++ infra/infra.tf
@@ -44,22 +44,22 @@ data "aws_route53_zone" "ziah_dev" {
 }
 
 
-resource "aws_route53_record" "ziah_dev" {
-  for_each = {
-    for dvo in aws_acm_certificate.ziah_dev.domain_validation_options : dvo.domain_name => {
-      name    = dvo.resource_record_name
-      record  = dvo.resource_record_value
-      type    = dvo.resource_record_type
-    }
-  }
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.ziah_dev.zone_id
-}
+#resource "aws_route53_record" "ziah_dev" {
+#  for_each = {
+#    for dvo in aws_acm_certificate.ziah_dev.domain_validation_options : dvo.domain_name => {
+#      name    = dvo.resource_record_name
+#      record  = dvo.resource_record_value
+#      type    = dvo.resource_record_type
+#    }
+#  }
+#
+#  allow_overwrite = true
+#  name            = each.value.name
+#  records         = [each.value.record]
+#  ttl             = 60
+#  type            = each.value.type
+#  zone_id         = data.aws_route53_zone.ziah_dev.zone_id
+#}
 
 resource "aws_acm_certificate_validation" "ziah_dev" {
   certificate_arn   = aws_acm_certificate.ziah_dev.arn
