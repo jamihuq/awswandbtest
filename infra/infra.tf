9f787255ee3aa8a1cf0b4bf9800ecb77028ea9a3
jamihuq <jamihuq2002@gmail.com>
2022-10-29 00:39:27 -0500

    added HTTPS listeners

--- infra/infra.tf
+++ infra/infra.tf
@@ -495,16 +495,6 @@ resource "aws_lb_target_group" "wandb_tg" {
   }
 }
 
-resource "aws_lb_listener" "wandb_listener" {
-  load_balancer_arn = aws_lb.wandb.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.wandb_tg.arn
-  }
-}
 
 # HTTP redirect listener 
 resource "aws_lb_listener" "wandb_listener" {
